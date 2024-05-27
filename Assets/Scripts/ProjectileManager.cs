20e327dd2e76a578970b16f92771eb054c11fc49
noble-shake <coppgabliw@gmail.com>
2024-05-27 20:05:52 +0900

    2024-05-27

--- Assets/Scripts/ProjectileManager.cs
+++ Assets/Scripts/ProjectileManager.cs
@@ -7,6 +7,7 @@ public class ProjectileManager : MonoBehaviour
 {
     [SerializeField] GameObject objCursor;
     Camera camMain;
+    
 
     // Start is called before the first frame update
     void Start()
